5baf2a8ec67982311f63c7c95751ca89a956441d
CaioPedroF <caio.pedrodez@gmail.com>
2026-01-29 17:14:03 -0300

    Codigo com Try catch e com CRUD e mudança na Pasta api-piloto

--- deloitte-bootcamp-dia02/Case01.cs
+++ deloitte-bootcamp-dia02/Case01.cs
@@ -1,5 +1,7 @@
 
 
+using System.Diagnostics;
+
 public class Produto
 
 {
@@ -15,17 +17,17 @@ public class Produto
       
       {
             Console.WriteLine("Nome nao pode ser Vazio");
-            return;
+            throw new Exception("Nao pode ser vazio!");
         }
     if (preco<=0)
         {
             Console.WriteLine("Preço deve ser maior que 0 ");
-            return;
+            throw new Exception("Nao pode ser menor ou igual a 0 ");
         }
     if(quantidade<0)
         {
             Console.WriteLine("nao pode ser negativa");
-            return;
+            throw new Exception("nao pode ser nulo");
         }
     
     Nome=nome;
@@ -38,32 +40,101 @@ public class Produto
 
 class Programa
 {
-    static void Main()
 
+     static List <Produto> estoque = new List<Produto>();
+     static void Main()
+        
     {
-        List<Produto> estoque = new List<Produto>();
-
-        Produto p1 = new Produto("Teclado",200,5);
-        Produto p2 = new Produto("",75,1);
-        Produto p3 = new Produto("Mouse",-34,12);
-
-        if (!string.IsNullOrEmpty(p1.Nome))
-        estoque.Add(p1);
-        if(!string.IsNullOrEmpty(p2.Nome))
-        estoque.Add(p2);
-        if(!string.IsNullOrEmpty(p3.Nome))
-        estoque.Add(p3);
-
-        Console.WriteLine("Produtos cadastrados");
-        foreach(var produto in estoque)
-        {
-            Console.WriteLine($"{produto.Nome} - R${produto.Preco} - qtd{produto.Quantidade}");
-        }
-    
+       while(true)
+      
+      
+       {
+            Console.WriteLine("\n1 Adicionar");
+            Console.WriteLine("2 Listar");
+            Console.WriteLine("3 Remover");
+            Console.WriteLine("0 Sair");
 
 
 
+            Console.WriteLine("Escolha");
+            string op = Console.ReadLine();
+
+            try
+            {
+                
+              switch(op)
+
+                {
+                    case "1":
+                      AdicionarProduto();
+                     break;
+                    case "2":
+                      Listar();
+                    break;
+                    case"3":
+                      Remover();
+                      break;
+                    case "0":
+                      return;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!" + ex.Message);
+             }
+
+         } 
+      
     }
+static void AdicionarProduto()
+    {
+        Console.Write("Nome:   ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Preço: ");
+        int preco = int.Parse(Console.ReadLine());
+
+        Console.Write("quantidade: ");
+        int qtd = int.Parse(Console.ReadLine());
+
+
+        Produto p = new Produto(nome,preco,qtd);
+
+        estoque.Add(p);
+        Console.WriteLine("Produto adicionado com Sucesso!");
+    }
+static void Listar()
+    {
+        Console.WriteLine("\n--- Estoque ---");
+
+        foreach(var p in estoque)
+
+        {
+            Console.WriteLine($"{p.Nome} - R${p.Preco}   - qtd{p.Quantidade}");
+        }
+
+        }
+static void Remover()
+
+        {
+            Console.WriteLine("Nome do produto para Remover:   ");
+            string nome = Console.ReadLine();
+
+
+            Produto p = estoque.Find(x => x.Nome == nome);
+
+            if (p == null)
+            {
+                Console.WriteLine("Produto não encontrado");
+                return;
+
+            }
+        
+            estoque.Remove(p);
+            Console.WriteLine("Produto Removido!!");
+        
+     }
 }
 
 
